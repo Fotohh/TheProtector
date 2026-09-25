e7cf461a5b0dcf997529779c4939fc9ac280baa0
Xaxis <zinha4304@gmail.com>
2026-09-24 22:40:12 -0700

    Rename main to .cc and add SDL renderer
    Rename src/main.cpp to src/main.cc and update CMakeLists.txt to build the .cc file. Initialize an SDL_Renderer, add error handling, set a blue draw color, clear and present the renderer each frame, and destroy the renderer on shutdown. Also adjust SDL include order to ensure SDL_main is included.

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <SDL3/SDL.h>
+
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
 
 int main(int, char*[]) {
 
@@ -17,22 +18,37 @@ int main(int, char*[]) {
 		return EXIT_FAILURE;
 	}
 
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+
+	if (renderer == nullptr) {
+		std::cerr << "Failed to create SDL Renderer: " << SDL_GetError() << "\n";
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+
 	bool running = true;
 
 	SDL_Event event;
 
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 	while (running) {
 		while (SDL_PollEvent(&event)) {
 			switch (event.type) {
-				case SDL_EVENT_QUIT: 
+				case SDL_EVENT_QUIT:
 					running = false;
 					break;
 				default:
 					break;
 			}
 		}
+		SDL_RenderClear(renderer);
+
+		//particle accelerator
+		SDL_RenderPresent(renderer);
 	}
-	
+
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
